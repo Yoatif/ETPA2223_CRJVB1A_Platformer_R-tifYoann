cc59161937cf0a917b0481d7675c2c34fd55d2eb
yoatif <y.retif@rns.ecolescreatives.com>
2023-05-12 09:01:35 +0200

    up

--- alpha/src/scene1.js
+++ alpha/src/scene1.js
@@ -13,77 +13,71 @@ class Scene1 extends Phaser.Scene{
         //preload diffent asset of the map
 
         //import tiles
-        this.load.tilemapTiledJSON("scene1", "../LD/onboarding.json");
+        this.load.tilemapTiledJSON("scene1", "./LD/onboarding.json");
 
         //import tileset
-        this.load.image("jeudetuile","../LD/tilesetAlpha.png");
+        this.load.image("jeudetuile","./LD/placeholder.png");
+
+        //import assets
+        this.load.image("collectible","./assets/collectible.png");
 
     }
 
     create(){
         console.log("first map")
-    //this.add.image(800,400,"sol");
 
-    // this.scene.add('Character', Character, true, { x: 400, y: 300 });
+        const carteDuNiveau = this.add.tilemap("scene1");
 
-    const carteDuNiveau = this.add.tilemap("scene1");
+        // importer les TileSet
+        const tileset = carteDuNiveau.addTilesetImage(
+        "placeholder",
+        "jeudetuile"
+        );
 
-    // importer les TileSet
-    const tileset = carteDuNiveau.addTilesetImage(
-    "tileset",
-    "jeudetuile"
-    );
+        const background = carteDuNiveau.createLayer(
+            "background",
+            tileset
+        );
 
-    const jump = carteDuNiveau.createLayer(
-    "jump",
-    tileset, 
-    );
+        const sol = carteDuNiveau.createLayer(
+        "sol",
+        tileset, 
+        );
 
-    const sol = carteDuNiveau.createLayer(
-    "sol",
-    tileset, 
-    );
-    
-
-    
-
-    //création caméra
-    this.cameras.main.setSize(1600, 900); 
-
-    this.player = this.physics.add.sprite(4750, 900, "hero_down")/*set position to  200, 6144, */;
-    this.player.setCollideWorldBounds(true);
+        this.collect = this.add.group();
+        this.collectibleLayer = carteDuNiveau.getObjectLayer('collectible');
+        this.collectibleLayer.objects.forEach(collectibleLayer => {
+            const collectItem = this.collect.create(collectibleLayer.x, collectibleLayer.y, "collectible")
+        });
 
 
 
-    //importation des entrées clavier
 
-    this.cursors = this.input.keyboard.createCursorKeys();
+        //importation des entrées clavier
 
-    //animation joueur
+        this.cursors = this.input.keyboard.createCursorKeys();
 
-                 
+        //animation joueur
 
-    
-    //set collision between player and encironement
-    
-    
+                    
+
+        
+        //set collision between player and encironement
+        
+        
 
 
-     // caméra 
-
-    this.cameras.main.setBounds(0,0,5120,3072);
-    this.cameras.main.startFollow(this.player);
-    this.physics.world.setBounds(0,0,5120,3072);
+        // caméra 
 
 
 
-    //input
-    this.cursors = this.input.keyboard.createCursorKeys();
+        //input
 
+                
     }
 
     update(){
 
       
-}
+    }
 }
